5ffedeee3e8fccd2bd286e2c3e29d96229d8e816
Khai <62913677+Khairul3232@users.noreply.github.com>
2024-07-29 11:51:42 -0600

    - works for ios device. show cases, the combination of materialicon, fontawesome icon, entypo, simplelineicon and ionicon.

--- App.tsx
+++ App.tsx
@@ -1,27 +1,44 @@
 import React from 'react';
-import { SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
+import {SafeAreaView, StatusBar, StyleSheet, Text, View} from 'react-native';
 import {Icons, IIcon} from './components/Icon';
 import {MaterialIcon} from './components/Icon';
+import {IconButton} from './components/IconButton.tsx';
 
 const App = () => {
   return (
-    <><StatusBar barStyle="dark-content" /><SafeAreaView style={styles.container}>
-      <View style={styles.container}>
-        <Text>This font-weight is 'regular' </Text>
-        <Text style={styles.boldFont}>This font-weight is 'bold' </Text>
-        {/* Add icons here */}
-        <MaterialIcon size="large" color="purple" name="home" />
-        <MaterialIcon size="extraLarge" color="black" name="github" />
-        <Text style={styles.titleText}>
-          Example of Ionicon Icon
-        </Text>
-        <IIcon
-          name="information-circle-outline"
-          size="extraLarge"
-          color="blue" />
-        <IIcon name="bicycle" size="extraLarge" color="red" />
-      </View>
-    </SafeAreaView></>
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.container}>
+        <View style={styles.container}>
+          <Text>This font-weight is 'regular' </Text>
+          <Text style={styles.boldFont}>This font-weight is 'bold' </Text>
+          <Text>The following shows </Text>
+          <Text>2 MaterialIcon and FontAwesomeIcon button icon</Text>
+          {/* Add icons here */}
+          <MaterialIcon size="large" color="purple" name="home" />
+          <MaterialIcon size="extraLarge" color="black" name="github" />
+          {/* Add icon button here */}
+          <IconButton
+            onPress={() => {}}
+            color="white"
+            size="extraLarge"
+            name="facebook"
+            text="Login in with Facebook"
+          />
+          <Text style={styles.titleText}>Example of Entypo Icon</Text>
+          <Icons.Entypo name="camera" size={24} color="black" />
+          <Text style={styles.titleText}>Example of SimpleLineIcon</Text>
+          <Icons.SimpleLineIcons name="plane" size={24} color="black" />
+          <Text style={styles.titleText}>Example of Ionicon Icon</Text>
+          <IIcon
+            name="information-circle-outline"
+            size="extraLarge"
+            color="blue"
+          />
+          <IIcon name="bicycle" size="extraLarge" color="red" />
+        </View>
+      </SafeAreaView>
+    </>
   );
 };
 export default App;
